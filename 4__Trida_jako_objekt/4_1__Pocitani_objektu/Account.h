586bb2728a94e41593070148748381fbe6d47b36
Jana Lyžbická <93248616+BochnikCZ@users.noreply.github.com>
2024-05-09 15:16:33 +0200

    Add files via upload

---
+++ 4__Trida_jako_objekt/4_1__Pocitani_objektu/Account.h
@@ -0,0 +1,40 @@
+#pragma once
+#include <iostream>
+#include <string.h>
+#include <stdbool.h>
+
+#include "Client.h"
+
+using namespace std;
+
+class Account
+{
+private:
+	static int objectsCountAccount;
+	int number;
+	double balance;
+	double interestRate;
+
+	Client* owner;
+	Client* partner;
+public:
+	static int GetObjectsCountAccount();
+
+	Account(int number, Client* c);
+	Account(int number, Client* c, double ir);
+	Account(int number, Client* c, Client* p);
+	Account(int number, Client* c, Client* p, double ir);
+	~Account();
+
+	int GetNumber();
+	double GetBalance();
+	double GetInterestRate();
+	Client* GetOwner();
+	Client* GetPartner();
+	bool CanWithdraw(double a);
+
+	void Deposit(double a);
+	bool Withdraw(double a);
+	void AddInterest();
+
+};
---
+++ 4__Trida_jako_objekt/4_1__Pocitani_objektu/Client.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <iostream>
+#include <string.h>
+#include <stdbool.h>
+
+
+using namespace std;
+
+
+
+class Client
+{
+private:
+	static int objectsCountClient;
+	int code;
+	string name;
+
+
+public:
+	static int GetObjectsCountClient();
+	
+	Client(int code, string name);
+	~Client();
+
+	int GetCode();
+	string GetName();
+};
